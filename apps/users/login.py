38b81a616d28c2025415f5a5032a45ff33c02d3e
islombek-develober <islombekbotiraliyev1@gmail.com>
2025-03-23 14:18:23 +0500

    edit users login serializers

--- apps/users/login.py
+++ apps/users/login.py
@@ -3,14 +3,7 @@ from .serializers import Loginserializers,RegistrationSerializer
 from rest_framework.permissions import AllowAny,IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
-
-
-
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework_simplejwt.tokens import RefreshToken
 
 class LoginAPIView(APIView):
     permission_classes = [AllowAny]
