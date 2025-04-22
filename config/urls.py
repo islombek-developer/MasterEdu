eb6f08c2bbbc1a828bdba512e7702e942fb0ca8e
islombek-develober <islombekbotiraliyev1@gmail.com>
2025-04-22 19:30:17 +0500

    add:serializers

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,11 @@ from django.conf.urls.i18n import set_language
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -32,4 +37,8 @@ urlpatterns = [
    path('swagger.<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+   # token
+   path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+   path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
